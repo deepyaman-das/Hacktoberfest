6ce4b45861a358ed707be56e1bf3036450111bad
Raunak Mandal <iamriju2000@gmail.com>
2021-10-09 15:07:28 +0530

    React app created

---
+++ lunaticoak/src/App.js
@@ -0,0 +1,15 @@
+import oak from "./oak.png";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={oak} className="App-logo" alt="logo" />
+        <h2>Lunatic Oak Website</h2>
+      </header>
+    </div>
+  );
+};
+
+export default App;
